7316f6f4b5011541e781b8e1f370f97e4dda6142
Jonirulah <Jonathanjoni12@hotmail.com>
2022-02-12 16:44:34 +0100

    Create en.lua
    Made by Biskfather#7679

---
+++ Shared/Locales/en.lua
@@ -0,0 +1,72 @@
+Locales['en'] = {
+
+    ['enter_house'] = 'Press ~INPUT_CONTEXT~ to enter your home',
+    ['view_house_info'] = 'Press ~INPUT_CONTEXT~ to see the information of this house',
+    ['exit_house'] = 'Press ~INPUT_CONTEXT~ to get out of the house',
+    ['open_storage'] = 'Press ~INPUT_CONTEXT~ to leave the house',
+    ['already_bought'] = 'This house already has an owner',
+    ['house_marker'] = 'House',
+
+    -- Buy Menu
+    ['house_menu'] = 'House menu',
+    ['house_info'] = 'See information of this house',
+    ['buy_house'] = 'Buy this house for',
+
+    -- Buy Menu - Notification
+    ['view_house_info_menu'] = 'house information',
+    ['house_id_menu'] = 'House ID: ',
+    ['house_price_menu'] = 'House Price: ',
+    ['interior_menu'] = 'House Type: ',
+
+    -- Buy Menu - Purchase
+    ['about_to_buy_house_menu'] = 'Buy house ',
+    ['about_to_sell_house_menu'] = 'Sell house ',
+    ['exceeded_house'] = 'You already have the maximum number of properties, you must sell some to buy this one.',
+
+    -- House Request Invite Menu
+    ['house_interaction'] = 'Press ~INPUT_CONTEXT~ to ring the bell',
+    ['invite_made'] = 'You have requested to enter the house ',
+
+    -- Police Actions
+    ['view_house_private_info'] = 'See private information of this house',
+
+    -- Bought house
+    ['bought_house'] = 'you bought the house ',
+
+    -- F5 Admin Menu
+    ['view_invites'] = 'See who has rung the doorbell',
+    ['sell_house'] = 'sell this house for',
+    ['invite_accepted'] = 'You just opened the door for ',
+    ['someone_ringing'] = 'someone is ringing at the door',
+
+    -- Keys Manager Menu
+    ['keys_manager'] = 'key management',
+    ['give_keys'] = 'give keys',
+    ['reset_keys'] = 'change the lock ',
+
+    -- Armory Menu
+    ['withdraw_items'] = 'Sacar items',
+    ['deposit_items'] = 'save items',
+    ['inventory'] = 'Inventory',
+    ['weapons'] = 'Weapons',
+    ['money'] = 'Money',
+    ['how_much_money'] = 'How much money do you want to save?',
+    ['how_many_items'] = 'How many items do you want to save?',
+    ['amount_invalid'] = 'invalid amount',
+    ['bullets'] = ' bullets',
+    ['already_has_weapon'] = 'You already have this weapon! Deposit your weapon to get this',
+    ['withdraw_success'] = 'have you taken out ',
+    ['deposit_success'] = 'you have saved ',
+
+    -- Menu Actions
+    ['yes'] = 'Yes',
+    ['no'] = 'No',
+
+    -- House Creation Menu (for admins)
+    ['house_builder'] = 'creation of houses',
+    ['interior_type'] = 'indoor type: ',
+    ['door_position'] = 'gate coordinates: ',
+    ['house_price'] = 'house price: ',
+    ['create_house'] = 'Confirm house creation',
+    ['house_created'] = 'You have successfully created the house '
+}
